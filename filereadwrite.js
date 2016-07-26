8f1164333cb1cd68ae36b49c223117f4e119a9ea
Aniket Lad <aniket.lad07@gmail.com>
2016-07-26 09:03:18 -0400

    Add files via upload

--- filereadwrite.js
+++ filereadwrite.js
@@ -9,7 +9,8 @@ window.onload = function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                alert(allText);
+                 fileDisplayArea.innerText = allText)
+                document.write("Hi trying to open a file");
             }
         }
     }
